a46cac50e2fb701e4b809deca48c6fa3955027d6
dc552 <dc552@DESKTOP-80ATPO4>
2024-02-12 19:51:10 -0400

    Agregar archivos de proyecto.

---
+++ Entrada_generos/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// definir un metodo que reciba por parametro la edad y el genero de la persona y devuelva un mensje
+
+Console.WriteLine("Ingrese su edad:");
+int edad = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingrese su género ");
+char genero = Convert.ToChar(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine();
+
+
+string mensaje = PedirDatos(edad, genero);
+Console.WriteLine(mensaje);
+
+Console.WriteLine();
+Console.WriteLine();
+
+
+static string PedirDatos(int edad, char genero)
+
+{
+
+    if (edad >= 18 && genero == 'f')
+    {
+        return "Puede pasar gratis.";
+    }
+    else if (edad >= 18 && genero == 'm')
+    {
+        return " Tiene que pagar para pasar.";
+    }
+    else
+    {
+        return "Eres menor, No puedes pasar";
+    }
+
+}
